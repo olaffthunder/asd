cde0462c69b8c7ef35a08791197cf7f6849c7788
olaffthunder <florian.stettmeier@gmx.de>
2018-02-18 02:03:50 +0100

    improved creation of Calories obejcts. Also added literals for creating a Calories object

--- dependencies/strong_types/strong_types.h
+++ dependencies/strong_types/strong_types.h
@@ -4,8 +4,8 @@ template <typename T, typename Parameter>
 class StrongType
 {
 public:
-    explicit StrongType(T const& value) : value_(value) {}
-    explicit StrongType(T&& value) : value_(std::move(value)) {}
+    constexpr explicit StrongType(T const& value) : value_(value) {}
+    constexpr explicit StrongType(T&& value) : value_(std::move(value)) {}
     T& get() { return value_; }
     T const& get() const { return value_; }
 private:
