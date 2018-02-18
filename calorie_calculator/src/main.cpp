eaae3783bba5e95c799a41600760c0a2fc57d41d
olaffthunder <florian.stettmeier@gmx.de>
2018-02-18 15:31:47 +0100

    improved header name

--- calorie_calculator/src/main.cpp
+++ calorie_calculator/src/main.cpp
@@ -1,4 +1,10 @@
+#include <iostream>
+
+#include "calorie_calculator/inc/algorithm.h"
+
 int main()
 {
+    calorie_calculator::Kilogramm weigth{72.8};
+    std::cout << calorie_calculator::harris_benedict_equation(weigth, 184, 25).get();
     return 0;
 }
